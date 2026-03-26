4ac2830ad47086ccf09718540af0b1545ead10ff
Dongho Han <dh1007@gmail.com>
2026-03-26 20:05:08 +0900

    Fix critical typo in shuffle algorithm

--- src/engine/aiPlayer.js
+++ src/engine/aiPlayer.js
@@ -31,7 +31,7 @@ export class AIPlayer {
     shuffle(array) {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[array[i]]];
+            [array[i], array[j]] = [array[j], array[i]];
         }
     }
 
